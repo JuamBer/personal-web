189c9ca16e5fadcdd3a057e07e96d455a02d098d
Juan Sáez García <saez@oscala.es>
2023-11-22 22:06:16 +0100

    style(Experiences): :dizzy: positionsHeaderEnterAnimation & positionsItemsEnterAnimation

--- src/app/website/components/experience/experience.component.ts
+++ src/app/website/components/experience/experience.component.ts
@@ -1,7 +1,18 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 import { Company } from 'src/app/backoffice/tables/company/models/company.model';
 import { Language } from 'src/app/backoffice/tables/language/models/language.model';
 import { Position } from 'src/app/backoffice/tables/position/models/position.model';
@@ -19,10 +30,27 @@ export class PositionGroupedByCompany {
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('positionsHeaderEnterAnimation', [
+      state('inViewport', style({ transform: 'translateX(0)' })),
+      state('notInViewport', style({ transform: 'translateX(+20%)' })),
+      transition('notInViewport <=> inViewport', animate('0.15s')),
+    ]),
+    trigger('positionsItemsEnterAnimation', [
+      state('inViewport', style({ transform: 'translateX(0)' })),
+      state('notInViewport', style({ transform: 'translateX(+20%)' })),
+      transition('notInViewport <=> inViewport', animate('0.25s')),
+    ]),
+  ],
 })
-export class ExperienceComponent implements OnInit {
+export class ExperienceComponent implements OnInit, AfterViewInit, OnDestroy {
   private store = inject(Store);
+  private ref = inject(ChangeDetectorRef);
 
+  @ViewChild('positions') positionsElement: ElementRef;
+  positionsElementState: 'inViewport' | 'notInViewport' = 'notInViewport';
+
+  unsubscribe$: Subject<void> = new Subject();
   language$: Observable<Language> = this.store.select(publicLanguageReducer.getOne);
   loadingPositions$: Observable<boolean> = this.store.select(positionReducer.getLoading);
   positionsGrouped$: Observable<PositionGroupedByCompany[]> = this.store.select(positionReducer.getAll).pipe(
@@ -59,10 +87,41 @@ export class ExperienceComponent implements OnInit {
     }),
   );
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          this.positionsElementState = entry.isIntersecting ? 'inViewport' : 'notInViewport';
+          if (this.positionsElementState === 'inViewport') {
+            this.ref.detectChanges();
+          }
+        });
+      });
+      observer.observe(this.positionsElement.nativeElement);
+    }, 100);
+  }
+
   ngOnInit(): void {
-    this.positionsGrouped$.subscribe((positionsGrouped) => {
+    this.positionsGrouped$.pipe(takeUntil(this.unsubscribe$)).subscribe((positionsGrouped) => {
       if (!positionsGrouped.length) {
         this.store.dispatch(positionActions.loadAll({ payload: null }));
+      }
+    });
+
+    this.loadingPositions$.pipe(takeUntil(this.unsubscribe$)).subscribe((loading) => {
+      if (!loading && this.positionsElementState === 'inViewport') {
+        this.positionsElementState = 'notInViewport';
+        this.ref.detectChanges();
+
+        setTimeout(() => {
+          this.positionsElementState = 'inViewport';
+          this.ref.detectChanges();
+        });
       }
     });
   }
