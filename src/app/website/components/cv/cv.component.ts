3ea36a54aaa2dcd7412a62d5917e9cdaf6e4a073
Juan Sáez García <saez@oscala.es>
2023-11-29 12:05:28 +0100

    refactor(CV): :globe_with_meridians: download cv from assets folder

--- src/app/website/components/cv/cv.component.ts
+++ src/app/website/components/cv/cv.component.ts
@@ -1,9 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { TranslateService } from '@ngx-translate/core';
-import { filter } from 'rxjs/operators';
-import { Language } from 'src/app/backoffice/tables/language/models/language.model';
-import { publicLanguageReducer } from 'src/app/shared/state/languages/public-language.reducer';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-cv',
@@ -11,23 +6,14 @@ import { publicLanguageReducer } from 'src/app/shared/state/languages/public-lan
   styleUrls: ['./cv.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CvComponent implements AfterViewInit {
-  private translateSrv = inject(TranslateService);
-  private store = inject(Store);
-
-  language!: Language;
-
-  ngAfterViewInit(): void {
-    this.store
-      .select(publicLanguageReducer.getOne)
-      .pipe(filter((i) => !!i))
-      .subscribe((language) => {
-        if (language) this.language = language;
-        if (language) this.translateSrv.use(language.acronym);
-      });
-  }
-
-  downloadCV(language: Language) {
-    window.open('https://drive.google.com/file/d/186ddhrvy_7H9-XsZIl7ZCFes8mkXLSQO/view?usp=drive_link', '_blank');
+export class CvComponent {
+  downloadCV() {
+    const link = document.createElement('a');
+    link.setAttribute('type', 'hidden');
+    link.href = 'assets/files/cv.pdf';
+    link.download = 'CV - JUAN SÁEZ GARCÍA';
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
 }
