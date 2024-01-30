7dd6d103cff54f43e144bae3f0966630931d3470
Juan Sáez García <saez@oscala.es>
2024-01-30 15:22:06 +0100

    ci(GitHubActions): :test_tube: fix error test

--- src/app/website/components/experience/experience.component.spec.ts
+++ src/app/website/components/experience/experience.component.spec.ts
@@ -60,10 +60,6 @@ describe('ExperienceComponent', () => {
     fixture.detectChanges();
   });
 
-  it('example error test', () => {
-    expect(false).toBe(true);
-  });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
