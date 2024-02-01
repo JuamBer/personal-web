8968dc421af73d2ae3948d144eba2cbadc08d3a5
Juan Sáez García <saez@oscala.es>
2024-02-01 16:12:18 +0100

    test(Language): :clown_face: generateOneMockLanguage()

---
+++ src/app/backoffice/tables/language/models/language.mock.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker';
+import { Language } from './language.model';
+
+export const generateOneMockLanguage = (): Language => ({
+  id: faker.string.uuid(),
+  nativeName: faker.lorem.word(),
+  name: faker.lorem.word(),
+  acronym: faker.lorem.word({ length: 2 }),
+  active: faker.datatype.boolean(),
+  createdAt: faker.date.past(),
+  updatedAt: new Date(),
+});
