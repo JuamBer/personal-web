f501daa06b8a204a61bc651db0439fd0d71edd29
Juan Sáez García <saez@oscala.es>
2024-02-01 15:36:24 +0100

    test(Layout): :test_tube: should create & should have app-sidebar & should have router-outlet

--- src/app/website/components/layout/layout.component.spec.ts
+++ src/app/website/components/layout/layout.component.spec.ts
@@ -1,1 +1,43 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LayoutComponent } from './layout.component';
 
+@Component({
+  selector: 'app-sidebar',
+  template: '',
+})
+class MockSidebarComponent {}
+
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LayoutComponent, MockSidebarComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have app-sidebar', () => {
+    const appSidebarElement = fixture.debugElement.query(By.css('app-sidebar'));
+    expect(appSidebarElement).toBeTruthy();
+  });
+
+  it('should have router-outlet', () => {
+    const routerOutletElement = fixture.debugElement.query(By.css('router-outlet'));
+    expect(routerOutletElement).toBeTruthy();
+  });
+});
